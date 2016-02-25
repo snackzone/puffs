84c2d7f94f90cbe5437a16e8c84c686eb22a7d00
Zachary Moroni <zachary.moroni@gmail.com>
2016-02-24 22:49:43 -0500

    Remove chmod script.

--- lib/puffs.rb
+++ lib/puffs.rb
@@ -1,7 +1,4 @@
 require_relative 'version'
 
-`'chmod +x bin/puffs'`
-`'chmod +x bin/rake'`
-
 project_root = File.dirname(File.absolute_path(__FILE__))
 Dir.glob(project_root + '/../app/models/*.rb') { |file| require file }
