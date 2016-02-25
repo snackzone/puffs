5f3db8ca1050bf7882643ebbd7e746fcaca23810
Zachary Moroni <zachary.moroni@gmail.com>
2016-02-25 14:19:06 -0500

    Change generate controller exe.

--- SnackTime/config/routes.rb
+++ SnackTime/config/routes.rb
@@ -3,7 +3,7 @@ ROUTER.draw do
   #Some sample routes:
 
   # get Regexp.new("^/cats/new$"), CatsController, :new
-  get Regexp.new("^/snacks/index$"), SnacksController, :new
+  get Regexp.new("^/snacks$"), SnacksController, :index
   # post Regexp.new("^/cats/create$"), CatsController, :create
   # get Regexp.new("^/cats$"), CatsController, :index
   # get Regexp.new("^/cats/(?<cat_id>\\d+)$"), CatsController, :show
