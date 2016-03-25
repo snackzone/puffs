a74c10db4230075ad837c43af08028ca01211199
Zachary Moroni <zachary.moroni@gmail.com>
2016-03-25 14:00:24 -0400

    Update SQLObject namespaces.

--- lib/sql_object/associatable.rb
+++ lib/sql_object/associatable.rb
@@ -1,97 +1,99 @@
 require 'active_support/inflector'
 
-class AssocOptions
-  attr_accessor(
-    :foreign_key,
-    :class_name,
-    :primary_key
-  )
+module Puffs
+  class AssocOptions
+    attr_accessor(
+      :foreign_key,
+      :class_name,
+      :primary_key
+    )
 
-  def model_class
-    class_name.constantize
-  end
+    def model_class
+      class_name.constantize
+    end
 
-  def table_name
-    model_class.table_name
-  end
-end
-
-class BelongsToOptions < AssocOptions
-  def initialize(name, options = {})
-    @primary_key = options[:primary_key] || :id
-    @foreign_key = options[:foreign_key] || "#{name}_id".to_sym
-    @class_name = options[:class_name] || name.to_s.capitalize
-  end
-end
-
-class HasManyOptions < AssocOptions
-  def initialize(name, self_class_name, options = {})
-    @primary_key = options[:primary_key] || :id
-    @foreign_key = options[:foreign_key] || "#{self_class_name.to_s.underscore}_id".to_sym
-    @class_name = options[:class_name] || name.to_s.singularize.camelcase
-  end
-end
-
-module Associatable
-  def belongs_to(name, options = {})
-    options = BelongsToOptions.new(name, options)
-    assoc_options[name] = options
-
-    define_method(name) do
-      foreign_key_value = self.send(options.foreign_key)
-      return nil if foreign_key_value.nil?
-
-      options.model_class
-             .where(options.primary_key => foreign_key_value)
-             .first
+    def table_name
+      model_class.table_name
     end
   end
 
-  def has_many(name, options = {})
-    options = HasManyOptions.new(name, self.to_s, options)
-    assoc_options[name] = options
-
-    define_method(name) do
-      target_key_value = self.send(options.primary_key)
-      return nil if target_key_value.nil?
-      options.model_class
-             .where(options.foreign_key => target_key_value)
-             .to_a
+  class BelongsToOptions < AssocOptions
+    def initialize(name, options = {})
+      @primary_key = options[:primary_key] || :id
+      @foreign_key = options[:foreign_key] || "#{name}_id".to_sym
+      @class_name = options[:class_name] || name.to_s.capitalize
     end
   end
 
-  def assoc_options
-    @assoc_options ||= {}
-  end
-
-  def has_one_through(name, through_name, source_name)
-    through_options = assoc_options[through_name]
-
-    define_method(name) do
-      source_options =
-        through_options.model_class.assoc_options[source_name]
-      through_pk = through_options.primary_key
-      key_val = self.send(through_options.foreign_key)
-
-      source_options.model_class.includes(through_options.model_class)
-                                .where(through_pk => key_val).first
+  class HasManyOptions < AssocOptions
+    def initialize(name, self_class_name, options = {})
+      @primary_key = options[:primary_key] || :id
+      @foreign_key = options[:foreign_key] || "#{self_class_name.to_s.underscore}_id".to_sym
+      @class_name = options[:class_name] || name.to_s.singularize.camelcase
     end
   end
 
-  def has_many_through(name, through_name, source_name)
-    through_options = assoc_options[through_name]
-    define_method(name) do
-      through_fk = through_options.foreign_key
-      through_class = through_options.model_class
-      key_val = self.send(through_options.primary_key)
+  module Associatable
+    def belongs_to(name, options = {})
+      options = BelongsToOptions.new(name, options)
+      assoc_options[name] = options
 
-      #2 queries, we could reduce to 1 by writing Puffs::SQLRelation.join.
-      through_class.where(through_fk => key_val)
-                   .includes(source_name)
-                   .load
-                   .included_relations
-                   .first
-                   .to_a
+      define_method(name) do
+        foreign_key_value = self.send(options.foreign_key)
+        return nil if foreign_key_value.nil?
+
+        options.model_class
+               .where(options.primary_key => foreign_key_value)
+               .first
+      end
+    end
+
+    def has_many(name, options = {})
+      options = HasManyOptions.new(name, self.to_s, options)
+      assoc_options[name] = options
+
+      define_method(name) do
+        target_key_value = self.send(options.primary_key)
+        return nil if target_key_value.nil?
+        options.model_class
+               .where(options.foreign_key => target_key_value)
+               .to_a
+      end
+    end
+
+    def assoc_options
+      @assoc_options ||= {}
+    end
+
+    def has_one_through(name, through_name, source_name)
+      through_options = assoc_options[through_name]
+
+      define_method(name) do
+        source_options =
+          through_options.model_class.assoc_options[source_name]
+        through_pk = through_options.primary_key
+        key_val = self.send(through_options.foreign_key)
+
+        source_options.model_class.includes(through_options.model_class)
+                                  .where(through_pk => key_val).first
+      end
+    end
+
+    def has_many_through(name, through_name, source_name)
+      through_options = assoc_options[through_name]
+      define_method(name) do
+        through_fk = through_options.foreign_key
+        through_class = through_options.model_class
+        key_val = self.send(through_options.primary_key)
+
+        #2 queries, we could reduce to 1 by writing Puffs::SQLRelation.join.
+        through_class.where(through_fk => key_val)
+                     .includes(source_name)
+                     .load
+                     .included_relations
+                     .first
+                     .to_a
+      end
     end
   end
 end
--- lib/sql_object/sql_object.rb
+++ lib/sql_object/sql_object.rb
@@ -4,154 +4,155 @@ require_relative '../relation'
 require 'active_support/inflector'
 # require_relative '../puffs'
 
-class Puffs::SQLObject
-  extend Associatable
+module Puffs
+  class SQLObject
+    extend Associatable
 
-  RELATION_METHODS = [
-    :limit, :includes, :where, :order
-  ]
+    RELATION_METHODS = [
+      :limit, :includes, :where, :order
+    ]
 
-  RELATION_METHODS.each do |method|
-    define_singleton_method(method) do |arg|
-      Puffs::SQLRelation.new(klass: self).send(method, arg)
-    end
-  end
-
-  def self.all
-    where({})
-  end
-
-  def self.columns
-    DBConnection.columns(table_name)
-  end
-
-  def self.count
-    all.count
-  end
-
-  def self.define_singleton_method_by_proc(obj, name, block)
-    metaclass = class << obj; self; end
-    metaclass.send(:define_method, name, block)
-  end
-
-  def self.destroy_all!
-    self.all.each do |entry|
-      entry.destroy!
-    end
-  end
-
-  def self.finalize!
-    self.columns.each do |column|
-      define_method(column) do
-        attributes[column]
-      end
-
-      define_method("#{column}=") do |new_value|
-        attributes[column] = new_value
+    RELATION_METHODS.each do |method|
+      define_singleton_method(method) do |arg|
+        Puffs::SQLRelation.new(klass: self).send(method, arg)
       end
     end
-  end
 
-  def self.find(id)
-    where(id: id).first
-  end
-
-  def self.first
-    all.limit(1).first
-  end
-
-  def self.has_association?(association)
-    assoc_options.keys.include?(association)
-  end
-
-  def self.last
-    all.order(id: :desc).limit(1).first
-  end
-
-  def self.parse_all(results)
-    relation = Puffs::SQLRelation.new(klass: self, loaded: true)
-    results.each do |result|
-      relation << self.new(result)
+    def self.all
+      where({})
     end
 
-    relation
-  end
+    def self.columns
+      Puffs::DBConnection.columns(table_name)
+    end
 
-  def self.table_name=(table_name)
-    @table_name = table_name
-  end
+    def self.count
+      all.count
+    end
 
-  def self.table_name
-    @table_name ||= self.to_s.downcase.tableize
-  end
+    def self.define_singleton_method_by_proc(obj, name, block)
+      metaclass = class << obj; self; end
+      metaclass.send(:define_method, name, block)
+    end
 
-  def initialize(params = {})
-    params.each do |attr_name, value|
-      unless self.class.columns.include?(attr_name.to_sym)
-        raise "unknown attribute '#{attr_name}'"
+    def self.destroy_all!
+      self.all.each do |entry|
+        entry.destroy!
+      end
+    end
+
+    def self.finalize!
+      self.columns.each do |column|
+        define_method(column) do
+          attributes[column]
+        end
+
+        define_method("#{column}=") do |new_value|
+          attributes[column] = new_value
+        end
+      end
+    end
+
+    def self.find(id)
+      where(id: id).first
+    end
+
+    def self.first
+      all.limit(1).first
+    end
+
+    def self.has_association?(association)
+      assoc_options.keys.include?(association)
+    end
+
+    def self.last
+      all.order(id: :desc).limit(1).first
+    end
+
+    def self.parse_all(results)
+      relation = Puffs::SQLRelation.new(klass: self, loaded: true)
+      results.each do |result|
+        relation << self.new(result)
       end
 
-      self.send("#{attr_name}=", value)
+      relation
     end
-  end
 
-  def attributes
-    @attributes ||= {}
-  end
-
-  def attribute_values
-    self.class.columns.map do |column|
-      self.send(column)
+    def self.table_name=(table_name)
+      @table_name = table_name
     end
-  end
 
-  def destroy!
-    if self.class.find(id)
-      DBConnection.execute(<<-SQL)
-        DELETE
-        FROM
+    def self.table_name
+      @table_name ||= self.to_s.downcase.tableize
+    end
+
+    def initialize(params = {})
+      params.each do |attr_name, value|
+        unless self.class.columns.include?(attr_name.to_sym)
+          raise "unknown attribute '#{attr_name}'"
+        end
+
+        self.send("#{attr_name}=", value)
+      end
+    end
+
+    def attributes
+      @attributes ||= {}
+    end
+
+    def attribute_values
+      self.class.columns.map do |column|
+        self.send(column)
+      end
+    end
+
+    def destroy!
+      if self.class.find(id)
+        Puffs::DBConnection.execute(<<-SQL)
+          DELETE
+          FROM
+            #{self.class.table_name}
+          WHERE
+            id = #{id}
+        SQL
+        return self
+      end
+    end
+
+    def insert
+      columns = self.class.columns.reject { |col| col == :id }
+      column_values = columns.map {|attr_name| send(attr_name)}
+      column_names = columns.join(", ")
+      bind_params = (1..columns.length).map {|n| "$#{n}"}.join(", ")
+      result = Puffs::DBConnection.execute(<<-SQL, column_values)
+        INSERT INTO
+          #{self.class.table_name} (#{column_names})
+        VALUES
+          (#{bind_params})
+          RETURNING id;
+      SQL
+      self.id = result.first['id']
+      self
+    end
+
+    def save
+      self.class.find(id) ? update : insert
+    end
+
+    def update
+      set_line = self.class.columns.map do |column|
+        "#{column} = \'#{self.send(column)}\'"
+      end.join(", ")
+
+      Puffs::DBConnection.execute(<<-SQL)
+        UPDATE
           #{self.class.table_name}
+        SET
+          #{set_line}
         WHERE
           id = #{id}
       SQL
-      return self
+      self
     end
-  end
-
-  def insert
-    columns = self.class.columns.reject { |col| col == :id }
-    column_values = columns.map {|attr_name| send(attr_name)}
-    column_names = columns.join(", ")
-    bind_params = (1..columns.length).map {|n| "$#{n}"}.join(", ")
-    result = DBConnection.execute(<<-SQL, column_values)
-      INSERT INTO
-        #{self.class.table_name} (#{column_names})
-      VALUES
-        (#{bind_params})
-        RETURNING id;
-    SQL
-    self.id = result.first['id']
-    self
-  end
-
-  def save
-    self.class.find(id) ? update : insert
-  end
-
-  def update
-    set_line = self.class.columns.map do |column|
-      #bobby tables says hi!
-      "#{column} = \'#{self.send(column)}\'"
-    end.join(", ")
-
-    DBConnection.execute(<<-SQL)
-      UPDATE
-        #{self.class.table_name}
-      SET
-        #{set_line}
-      WHERE
-        id = #{id}
-    SQL
-    self
   end
 end
